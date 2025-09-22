8ef3486d4de17fc8589cee0baed38a0d8dd94db7
Dominik Moritz <domoritz@gmail.com>
2025-09-21 21:10:19 -0400

    bar chart component (empty)

---
+++ src/lib/BarChart.svelte
@@ -0,0 +1,14 @@
+<script module>
+	// module-level logic goes here
+	// (you will rarely use this)
+</script>
+
+<script lang="ts">
+	// instance-level logic goes here
+</script>
+
+<!-- markup (zero or more items) goes here -->
+
+<style>
+	/* styles go here */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+	import BarChart from "$lib/BarChart.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h2>Charts</h2>
+<BarChart/>
